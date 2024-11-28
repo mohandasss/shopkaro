3df043a2c0a21986d16cc509d322ad64feadfb87
mohandasss <gamermohan39@gmail.com>
2024-11-28 13:13:10 +0530

    mobile navigation fixed

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -113,23 +113,27 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
-              {item.icon}
-              <span className="sr-only">{item.name}</span>
-            </DisclosureButton>
-          ))}
+  <div className="space-y-1 px-2 pb-3 pt-2">
+    {navigation.map((item) => (
+      <DisclosureButton
+        key={item.name}
+        as="a"
+        href={item.href}
+        className={classNames(
+          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+          'block rounded-md px-3 py-2 text-base font-medium'
+        )}
+      >
+        {/* Center the icon horizontally */}
+        <div className="flex justify-center items-center">
+          {item.name}
         </div>
-      </DisclosurePanel>
+       
+      </DisclosureButton>
+    ))}
+  </div>
+</DisclosurePanel>
+
 
       {/* Support Modal */}
       {modal === 'support' && (
